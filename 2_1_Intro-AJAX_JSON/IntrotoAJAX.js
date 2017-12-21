32af73fdea3a572ec8703ea81c707d5ee89687ef
Noly <franco.noly@gmail.com>
2017-12-21 07:32:41 +0000

    testing

---
+++ 2_1_Intro-AJAX_JSON/IntrotoAJAX.js
@@ -0,0 +1,52 @@
+/*
+            What is AJAX?
+
+Asynchronous
+JavaScript
+and 
+XML
+
+    AJAX IS NOT 
+    1. A Library
+    2. A Framework
+    3. A Technology
+    
+    AJAX is...
+    an approach(that's it)
+***Back in 2005ish***
+        HTML
+        JavaScript
+        The DOM
+        CSS
+        XMLHTTP Requests
+            What is boils down to 
+        with AJAX, websites can send and request data from a sever in
+        the background without disturbing the current page
+        
+        What is boils down to 
+        with AJAX, websites can send and request data
+        from a server in the background without disturbing the current page
+        
+        
+        JavaScript
+        "The user scrolled to the bottom!"
+        
+        "Quick, send a request to the server and get more content!"
+        
+        "Now I'll just append the new 
+        data to the bottom of the page"
+        
+        Making Requests with JavaScript
+        1.xMLMGHTTP Request
+        2.The Fetch API
+        3.3rd Party Libraries;
+        JQuery, Axios, etc.
+        
+        Data 
+        formats
+        
+        XML AND JavaScript Object Notation(JSON)--> Data Formats
+        JSON looks almost exactly like JavaScript objects
+        JSon is more commoly used these days because it works so well with Javascript
+*/
+
